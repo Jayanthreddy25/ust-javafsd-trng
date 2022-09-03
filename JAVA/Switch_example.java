a4d04ec64de577faeee7956fbd026b7347f1ee05
Jayanthreddy25 <112387758+Jayanthreddy25@users.noreply.github.com>
2022-09-03 09:16:52 +0530

    Add files via upload

---
+++ JAVA/Switch_example.java
@@ -0,0 +1,25 @@
+package com.ust;
+
+public class Switch_example {
+public static void main(String args[]) {
+	String str="Green1";
+	switch(str) {
+	case "red":
+		System.out.println("im in red");
+		System.out.println("*");
+	default:
+		System.out.println("default");
+		break;
+	case "Green":
+		System.out.println("iam in green");
+		System.out.println("*");
+		break;
+	case "yellow":
+		System.out.println("iam in yellow");
+		System.out.println("*");
+		break;
+		
+		
+	}
+}
+}
