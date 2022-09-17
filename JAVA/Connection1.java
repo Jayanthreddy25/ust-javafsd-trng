14956f6dd348c32e276eac4d98b4b7cf30aa9ee7
Jayanthreddy25 <112387758+Jayanthreddy25@users.noreply.github.com>
2022-09-17 09:30:12 +0530

    Add files via upload

---
+++ JAVA/Connection1.java
@@ -0,0 +1,28 @@
+package Enumerations;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Connection1 {
+
+	public static void main(String[] args) {
+		try {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ust","root","root");
+		Statement s=con.createStatement();
+		ResultSet rs=s.executeQuery("select * from Employee");
+		while(rs.next()) {
+			System.out.print(rs.getInt(1)+" ");
+			System.out.print(rs.getInt(2)+" ");
+			System.out.println(rs.getString(3)+" ");
+		}
+		
+		con.close();
+	}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
