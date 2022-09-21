6b089619e84db2c5b3e4ba0e9c5656fc71d51974
Jayanthreddy25 <112387758+Jayanthreddy25@users.noreply.github.com>
2022-09-21 15:15:36 +0530

    Add files via upload

---
+++ JAVA/OptionalClassExample3.java
@@ -0,0 +1,18 @@
+package Methodreference;
+
+import java.util.Optional;
+
+public class OptionalClassExample3 {
+
+	public static void main(String[] args) {
+
+		Optional<String> emptyOption = Optional.empty();
+
+		Optional<String> strOption = Optional.of("one");
+
+		System.out.println(emptyOption.orElseGet(() -> " optional null orElseGet"));
+
+		System.out.println(strOption.orElseGet(() -> "Optional value orElseGet"));
+
+		}
+}
